8b70374e4caa3493db10279d5bf79d6f0f59cbf0
wsshj <wsshj159@gmail.com>
2021-05-28 17:57:01 +0800

    update

--- Python_Excel/云极视界/excel.py
+++ Python_Excel/云极视界/excel.py
@@ -1,10 +1,16 @@
-from numpy import False_
+import numpy as np
 import pandas as pd
 import os
+import json
 
-judge_list = []
+
+judge_list = ["未指定"]
 staff_list = []
 score_list = []
+judge_dict = {}
+staff_dict = {}
+
+staff_id = 0
 
 def get_file_path(folderName):
     file_path_list = []
@@ -34,7 +40,7 @@ class Execl():
     def get_judge(self):
         for col in range(5,8):
             for row in range(4, self.rows-4):
-                if self.data_frame.iloc[row,col] == "" :
+                if self.data_frame.iloc[row,col] is np.nan :
                     continue
 
                 if self.data_frame.iloc[row,col] in judge_list:
@@ -42,9 +48,7 @@ class Execl():
 
                 judge_list.append(self.data_frame.iloc[row,col])
 
-    def get_score(self):
-        score_dict = {}
-
+    def get_score(self, staff_id):
         str = self.data_frame.iloc[1,0].split('：' or ':')
         for s in str[1].split(' '):
             if(s != ''):
@@ -58,48 +62,115 @@ class Execl():
         
         name = str[3]
 
-        for row in range(4, self.rows-15):
-            for col in range(5,8):
-                if self.data_frame.iloc[row,col] == "" :
-                    
+        for row in range(4, self.rows-4):
+            judge_num = 3
+            ratio = 0
+            if self.data_frame.iloc[row, 7] is np.nan :
+                judge_num -= 1
 
-                score_dict["编号"] = 
+            if self.data_frame.iloc[row, 6] is np.nan :
+                judge_num -= 1
+
+            for col in range(1, judge_num+1):
+                score_dict = {}
+
+                if judge_num == 3:
+                    if col == 1:
+                        ratio = 0.5
+                    elif col == 2:
+                        ratio = 0.3
+                    else:
+                        ratio = 0.2
+                
+                if judge_num == 2:
+                    if col == 1:
+                        ratio = 0.5
+                    else:
+                        ratio = 0.5
+                
+                if judge_num == 1:
+                    ratio = 1
+
+                # score_dict["编号"] = staff_id
+
                 score_dict["部门"] = department
                 score_dict["岗位"] = post
                 score_dict["姓名"] = name
+
                 score_dict["项目"] = self.data_frame.fillna(method='ffill').iloc[row,1]
-                score_dict["内容"] = self.data_frame.fillna(method='ffill').iloc[row,2]
-                score_dict["指标获得满分的标准"] = self.data_frame.fillna(method='ffill').iloc[row,3]
-                score_dict["权重"] = self.data_frame.fillna(method='ffill').iloc[row,4]
-                score_dict["占比"] = ""
+                score_dict["内容"] = self.data_frame.iloc[row,2] if not self.data_frame.iloc[row,2] is np.nan else ""
+                score_dict["指标获得满分的标准"] = self.data_frame.iloc[row,3] if not self.data_frame.iloc[row,3] is np.nan else ""
+                score_dict["权重"] = "%s%%" % round(float(self.data_frame.iloc[row,4]) * 100) if not self.data_frame.iloc[row,4] is np.nan else ""
+                
+                score_dict["考核人"] = self.data_frame.iloc[row,4+col]
+
+                score_dict["占比"] = ratio
                 score_dict["分数"] = ""
         
-        score_list.append(score_dict)
+                score_list.append(score_dict)
 
-    def handle_execl(self):
+    def handle_execl(self, staff_id):
         for i in range(0, self.sheet_len):
-            self.data_frame = pd.read_excel(self.file_path, sheet_name=i, index_col=None, header=None, keep_default_na=False)
+            self.data_frame = pd.read_excel(self.file_path, sheet_name=i, index_col=None, header=None)#, keep_default_na=False)
+            staff_id += 1
+            
             self.rows = self.data_frame.shape[0]
             self.cols = self.data_frame.columns.size
             self.get_staff()
             self.get_judge()
-            self.get_score()
+            self.get_score(staff_id)
+
+    def screen_score(self): 
+        for judge in judge_list:
+            jl = []
+            for score in score_list:
+                if score.get("考核人") == judge:
+                    jl.append(score)
+                    # score_list.remove(score)
+
+            judge_dict[judge] = jl
+
+        wzd = []
+        for score in score_list:
+            if score.get("考核人") is np.nan:
+                wzd.append(score)
+                # score_list.remove(score)
+
+        judge_dict["未指定"] = wzd
+
+def print_execl(folder_path):
+    for key,value in judge_dict.items():
+        file_name = '\\%s.xlsx' % key
+        outDataFrame = pd.DataFrame(value)
+        outDataFrame.to_excel(folder_path + file_name,
+                            sheet_name='Sheet1',
+                            startcol=0,
+                            index=False)
 
 if __name__ == "__main__":
     folder_path = os.path.dirname(os.path.realpath(__file__)) + '\\assessment'
-    # folder_path = os.path.dirname(os.path.realpath(__file__)) + '\\assessment'
+    print_folder_path = os.path.dirname(os.path.realpath(__file__)) + '\\print'
+    config_folder_path = os.path.dirname(os.path.realpath(__file__)) + '\\config'
 
     for file_path in get_file_path(folder_path):
+        print(file_path)
         if not check_file_type(file_path):
             print('File:%s is not Excel!' % file_path)
             continue
 
         execl = Execl(file_path)
-        execl.handle_execl()
+        execl.handle_execl(staff_id)
+        execl.screen_score()
 
-    print(staff_list)
-    print(score_list)
-    print(judge_list)
+    print_execl(print_folder_path)
 
+    staff_dict["staff"] = staff_list
+    with open(config_folder_path + "\\staff.json", "w" , encoding='utf8') as f:
+        json.dump(staff_dict,f)
+
+        # print(judge_dict)
+        # print(score_list)
+        # print(judge_list)
+        
 
     
---
+++ Python_Excel/云极视界/total.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import os
+import json
+
+staff_list = []
+info_list = []
+score_list = []
+
+def get_file_path(folderName):
+    file_path_list = []
+    
+    for root, dirs, files in os.walk(folderName):
+        for name in files:
+            file_path_list.append(os.path.join(root, name))
+    
+    return file_path_list
+
+def check_file_type(file_name):
+    extensions = ['.xlsx', '.xls']
+
+    return any(file_name.endswith(extension) for extension in extensions)
+
+def open_json(file_name):
+    with open(file_name,'r') as load_f:
+        return  json.load(load_f).get("staff")
+
+def count():
+    for staff in staff_list:
+        score = 0
+        bm = ''
+        gw = ''
+        for info in info_list:
+            if staff == info["xm"]:
+                score += info["df"]
+                bm = info["bm"]
+                gw = info["gw"]
+
+        d = {}
+        d["部门"] = bm
+        d["岗位"] = gw
+        d["姓名"] = staff
+        d["得分"] = score
+
+        score_list.append(d)
+
+def print_execl(file_name):
+    outDataFrame = pd.DataFrame(score_list)
+    outDataFrame.to_excel(file_name + "\\总分.xlsx",
+                        sheet_name='Sheet1',
+                        startcol=0,
+                        index=False)  
+
+class Execl():
+    def __init__(self, file_path) -> None:
+        self.file_path = file_path
+        self.data_frame = pd.read_excel(self.file_path, sheet_name=0, index_col=None, header=None)
+        self.rows = self.data_frame.shape[0]
+        self.cols = self.data_frame.columns.size
+
+    def get_data(self):
+        for row in range(1, self.rows):
+            d = {}
+            d["bm"] = self.data_frame.iloc[row,0]
+            d["gw"] = self.data_frame.iloc[row,1]
+            d["xm"] = self.data_frame.iloc[row,2]
+
+            d["qz"] = self.data_frame.iloc[row,6] if not self.data_frame.iloc[row,6] is np.nan else 0
+            d["zb"] = self.data_frame.iloc[row,8] if not self.data_frame.iloc[row,8] is np.nan else 0
+            d["fs"] = self.data_frame.iloc[row,9] if not self.data_frame.iloc[row,9] is np.nan else 0
+
+            d["df"] = d["fs"] * d["zb"]# * d["qz"]
+        
+            info_list.append(d)  
+
+if __name__ == "__main__":
+    folder_path = os.path.dirname(os.path.realpath(__file__)) + '\\print'
+    print_folder_path = os.path.dirname(os.path.realpath(__file__)) + '\\total'
+
+    staff_list = open_json(os.path.dirname(os.path.realpath(__file__)) + "\\config\\staff.json")
+
+    for file_path in get_file_path(folder_path):
+        if not check_file_type(file_path):
+            print('File:%s is not Excel!' % file_path)
+            continue
+
+        execl = Execl(file_path)
+        execl.get_data()
+
+    count()
+    print_execl(print_folder_path)
+
+    # print(info_list)
+    print(score_list)
+    
